5d1519dfcdf3c3f4cee1883b816384561f41f617
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-06-07 21:39:28 +0300

    Added get all restaurants api

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RestaurantController;
 
 
+
+Route::get('/restaurants/{id?}', [RestaurantController::class, 'getAllRestaurants']);
+Route::post('/add_resto', [RestaurantController::class, 'addResto']);
